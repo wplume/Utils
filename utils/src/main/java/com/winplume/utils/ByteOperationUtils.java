074e41decc7de06a1dde685a5be80114f02bc895
WindPlume <windplume.mail@gmail.com>
2020-03-11 11:51:55 +0800

    2020年3月11日，ByteOperationUtils增加byte[]转十六进制的字符串

--- utils/src/main/java/com/winplume/utils/ByteOperationUtils.java
+++ utils/src/main/java/com/winplume/utils/ByteOperationUtils.java
@@ -4,4 +4,14 @@ public class ByteOperationUtils {
     public static int func(byte height, byte low) {
         return height << 8 + low;
     }
+
+
+    public static String hexByteArrayToHexString(byte[] bytes) {
+        StringBuilder buf = new StringBuilder(bytes.length * 2);
+        for (byte b : bytes) {
+            //以十六进制长度为2的形式输出
+            buf.append(String.format("%02x", b & 0xff)).append(" ");
+        }
+        return buf.toString();
+    }
 }
